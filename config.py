0beea15c5a02610a258a3d7a29979503cf9a063d
Tulasinathreddy <34623886+Tulasinathreddy@users.noreply.github.com>
2020-10-23 14:20:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,55 @@
+#bot details =====================================================================================================
+token = "11111111:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx" #Telegram bot token
+
+# wapi =====================================================================================================
+# register in wapi website to get this key link : https://mrcyjanek.net/wapi/ or contact t.me/@mrcyjanek
+api_key = "ooooooooooooooo00000000000ooooooooooooo"
+
+# register in wapi website to get this key link : https://mrcyjanek.net/wapi/ or contact t.me/@mrcyjanek
+wapi_username = 'lllllllll'
+# wapi end =====================================================================================================
+
+# database =====================================================================================================
+#your MySql database host
+db_host = "localhost"
+
+#your MySql database username
+db_user = "xxxxxxxx"      
+
+#your MySql database password
+db_pass = "xxxxxxxx"
+
+#your MySql database database name
+db_database = "xxxxxxxx"
+# database end =====================================================================================================
+
+# config details  =====================================================================================================
+# minimum withdraw ( keep them all in decimals only ) 
+ltc_min_withdraw = 0.00002280   #example : 0.0001
+doge_min_withdraw = 1.00        #example : 1.000
+btc_min_withdraw = 0.00000100   #example : 0.00001
+
+#admin id
+adminid = 1152662911
+
+#info button message
+bot_info = "<b>Welcome To Multi Tip Bot</b>\n\n<b>Commands :</b>\n💦 <code>/rain 10 doge</code> - Raining In a Group to all the active users\n🎉 <code>/tip 10 doge</code> - Reply To a users message whom you want to tip/transfer/send your balance\n⚡ <code>/active</code> - Send You the number of active users currently\n<i>All the above mentioned command are only allowed in Group</i>\n\nThis Bot is made by @mrbeandev\n<i>This is only a trail bot</i>To Add this type of a bot to Your Group contact Admin"
+
+# bot name or any contant which will be displayed on start message
+bot_name = "<b>Welcome To tip bot</b>"   # this supports html markups
+
+#dont change this if you dont understand what it is !!
+active_users_store_time = 300.00 # i recommend not to change this value XXXXXXXXXX
+
+#menu =====
+# balance button
+button1 = "💰 Balance 💰"
+
+# deposit button
+button2 = "📥 Dump 📥"
+
+# withdraw button
+button3 = "📤 Payout 📤"
+
+# info button
+button4 = "🛡 Info 🛡"
